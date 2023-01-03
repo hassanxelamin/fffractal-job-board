7c9141ea583b5f88dda4a172f1c579db6c8f7be3
hassanxelamin <accounts@hassanxelamin.com>
2023-01-03 11:15:16 -0800

    styled searchbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { TitleSection, EmailForm } from 'src/components';
+import { TitleSection, EmailForm, SearchBar } from 'src/components';
 
 const Home: NextPage = () => {
   return (
@@ -21,7 +21,12 @@ const Home: NextPage = () => {
       <div className="flex flex-col justify-center items-center overflow-hidden">
         <EmailForm />
       </div>
+      <div className="flex items-center justify-center">
+        <SearchBar />
+      </div>
+      {/* <div> */}
       {/* <Jobs /> */}
+      {/* </div> */}
     </div>
   );
 };
